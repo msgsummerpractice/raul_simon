f2cf8b0e510faf966d45c38fccc97625cfdbf681
Raul Simon <raulsimonalexas@yahoo.com>
2026-07-29 13:45:59 +0300

    Pull request issued resolved

--- JavaScript/dogGallery.js
+++ JavaScript/dogGallery.js
@@ -1,50 +1,39 @@
-const heading = document.createElement('h1');
-heading.textContent = 'Random Dog Gallery';
-document.getElementById("container").appendChild(heading);
-
 async function getDogImage() {
   try {
-    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error('Failed to fetch dog image:', error);
+    console.error("Failed to fetch dog image:", error);
   }
 }
 
-const button = document.createElement('button');
-button.id = 'myButton';
-const btnText = document.createElement('span');
-btnText.id = 'btnText';
-btnText.textContent = 'Show me a dog!';
-button.appendChild(btnText);
-document.getElementById("container").appendChild(button);
+const button = document.getElementById("myButton");
+const btnText = document.getElementById("btnText");
+btnText.textContent = "Show me a dog!";
 
-    button.addEventListener('click', () => {
-        button.disabled = true;
+button.addEventListener("click", () => {
+  button.disabled = true;
 
-        btnText.textContent = 'Loading...';
+  btnText.textContent = "Loading...";
 
-        const spinner = document.createElement('div');
-        spinner.classList.add('spinner');
-        button.appendChild(spinner);
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "inline";
 
-        setTimeout(() => {
-            button.disabled = false;
-            btnText.textContent = 'Show me a dog!';
-            spinner.remove();
-          let dogImage = document.querySelector('#container img');
+  setTimeout(() => {
+    button.disabled = false;
+    btnText.textContent = "Show me a dog!";
+    spinner.style.display = "none";
+    let dogImage = document.getElementById("dogImage");
 
-        if (!dogImage) {
-          dogImage = document.createElement('img');
-          dogImage.alt = 'Random Dog';
-          document.getElementById("container").insertBefore(dogImage, button);
-        }
+    if (!dogImage) {
+      dogImage = document.createElement("img");
+      document.getElementById("container").insertBefore(dogImage, button);
+    }
 
-        getDogImage().then(data => {
-          dogImage.src = data.message;
-        });
-        }, 3000);
-        
+    getDogImage().then((data) => {
+      dogImage.src = data.message;
+      dogImage.style.display = "block";
     });
-
+  }, 3000);
+});

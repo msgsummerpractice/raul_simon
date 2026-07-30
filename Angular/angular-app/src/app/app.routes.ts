d1a91111da950eb62031ba24401250d7c6e599b8
Raul Simon <raulsimonalexas@yahoo.com>
2026-07-30 16:56:24 +0300

    Redirect user to '/home' by default

--- Angular/angular-app/src/app/app.routes.ts
+++ Angular/angular-app/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { App } from './app';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   {
     path: 'login',
